7215e1964d237265cc388d94060822bc7c360c5a
THETASOLA <tao.solan@gmail.com>
2022-10-28 09:59:22 +0200

    Getting somewhere with GetArgFromLine

---
+++ cmake-build-debug/CMakeFiles/Projet.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet.dir/main.c.obj"
+  "CMakeFiles/Projet.dir/main.c.obj.d"
+  "CMakeFiles/Projet.dir/read.c.obj"
+  "CMakeFiles/Projet.dir/read.c.obj.d"
+  "Projet.exe"
+  "Projet.exe.manifest"
+  "Projet.pdb"
+  "libProjet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

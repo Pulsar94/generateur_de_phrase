555d80bd7aeed909f4b94c335c94af8d0f301104
Pulsar94 <94862548+Pulsar94@users.noreply.github.com>
2022-11-17 09:31:05 +0100

    pre-merge

---
+++ generator.c
@@ -0,0 +1,30 @@
+//
+// Created by Tao S on 11/12/2022.
+//
+
+#include "generator.h"
+
+//Generate three binary trees (name,verb,adjective) from specified file dictionary
+void generate_three_type_tree(tree nam_tree,tree adj_tree,tree ver_tree){
+    char *prev_word;
+    FILE* file = fopen(TEXT,"r");
+    prev_word = GetArgFromLine(2,file)->word2;
+    printf("Generating trees...\n");
+    for(int i=0;i<TEXT_LENGTH;i++){ //Row size of the dictionary file
+        p_dic new = GetArgFromLine(i,file);
+        if(strcmp(prev_word,new->word2) != 0) {
+            strcpy(prev_word,new->word2);
+            if (GetType(new) == 1) { //Testing what is the type of the word extracted to assign him the corresponding tree
+                add_word(nam_tree->root, new->word2, new->num2);
+            }
+            if (GetType(new) == 2) {
+                add_word(adj_tree->root, new->word2, new->num2);
+            }
+            if (GetType(new) == 3) {
+                add_word(ver_tree->root, new->word2, new->num2);
+            }
+        }
+    }
+    fclose(file);
+    printf("Trees Generated\n");
+}
